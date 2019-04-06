6880fc4d385be8d6235e4e66a7c1bfa68fc05cea
diegorugue <diegorugue>
2019-04-06 13:32:30 -0300

    Atualizado

--- troca-almoco-master/config/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'TPdkr9mxG4GooTQQ7C6rsCBD0V3e7ELd';
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Cardapio SMN!';
-global.EMAIL_TMPL2 = 'Olá, o usuario <strong>{0}</strong>, trocou o prato principal para <strong>{1}</strong>';
-
-module.exports = {
-    connectionString: 'mongodb+srv://smncardapio:smncardapio@cluster0-9hq1p.azure.mongodb.net/test?retryWrites=true',
-    sendgridKey: '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'
-}
